da76cc37d7ceaa88a93c2ec320bbe76528b1f426
Dipankar Arora <dipankar.a511@gmail.com>
2023-11-16 23:15:39 -0600

    add to cart and food menu

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import ProductDetails from "./pages/ProductDetails";
-import Cart from "./pages/Cart";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Dashboard from "./pages/dashboard/Dashboard";
-import ProductManage from "./pages/dashboard/ProductManage";
-import Products from "./components/products/Products";
-import ProductDashboard from "./pages/dashboard/ProductDashboard";
-import AddProduct from "./pages/dashboard/AddProduct";
-import EditProduct from "./pages/dashboard/EditProduct";
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 8181;
+const productRoute = require("./routes/productRoute");
+const connectDB = require("./db/connect");
+const notFoundError = require("./middleware/notFoundError");
+const customMiddlewareErrorHandler = require("./middleware/customMiddlewareErrorHandler");
+const app = express();
 
-function App() {
-  return (
-<<<<<<< HEAD
-    <div className="container">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="product/:id" element={<ProductDetails />} />
-        </Route>
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<Dashboard />}>
-          <Route index element={<h2>Dashboard</h2>} />
-          <Route path="product" element={<ProductManage />}>
-            <Route index element={<ProductDashboard />} />
-            <Route path="new" element={<AddProduct />} />
-            <Route path=":id" element={<EditProduct />} />
-          </Route>
-          <Route path="settings" element={<h2>settings</h2>} />
-          <Route path="logout" element={<h2>logout</h2>} />
-        </Route>
-      </Routes>
-=======
-    <div>
-     <h1>Food Ordering - Foody App check deploymentnnn</h1>
->>>>>>> 69ecd4bafff2ac8c95144aa6eccf7de9c995aee1
-    </div>
-  );
-}
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-export default App;
+app.use("/api/v1/product/", productRoute);
+
+app.use(notFoundError);
+app.use(customMiddlewareErrorHandler);
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server started listening at port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+start();
