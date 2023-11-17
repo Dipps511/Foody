518b96dd405895d31d72d189ba2748605078452c
Dipankar Arora <58307044+Dipps511@users.noreply.github.com>
2023-11-17 00:34:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-require("dotenv").config();
-require("express-async-errors");
-const express = require("express");
-const cors = require("cors");
-const PORT = process.env.PORT || 8181;
-const productRoute = require("./routes/productRoute");
-const connectDB = require("./db/connect");
-const notFoundError = require("./middleware/notFoundError");
-const customMiddlewareErrorHandler = require("./middleware/customMiddlewareErrorHandler");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.get('/', (req, res) =>{
+    res.json(
+        {
+            mesg:'Food Ordering - Backend...'
+        })
+})
 
-app.use("/api/v1/product/", productRoute);
-
-app.use(notFoundError);
-app.use(customMiddlewareErrorHandler);
-
-const start = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`Server started listening at port ${PORT}`);
-    });
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-start();
+app.listen(3000)
